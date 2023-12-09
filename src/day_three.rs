2991d6ae7d65de9b6f022d08d7ca69d46950d7a9
David Hayes <dmhayes82@gmail.com>
2023-12-09 17:35:02 +0000

    Day three part 2

--- src/day_three.rs
+++ src/day_three.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 
 pub(crate) fn day_three_part_one(input: &str) -> Result<u32, std::io::Error> {
 
@@ -27,6 +28,41 @@ pub(crate) fn day_three_part_one(input: &str) -> Result<u32, std::io::Error> {
         Err(std::io::Error::new(std::io::ErrorKind::Other, "No part found"))
     } else {
         Ok(part_sum)
+    }
+}
+
+pub(crate) fn day_three_part_two(input: &str) -> Result<u32, std::io::Error> {
+    let mut calibration_sum = 0;
+    let mut potential_gear_map: HashMap<(usize, usize), u32> = HashMap::new();
+    for y in 0..input.lines().count() {
+        let char_count = input.lines().nth(y).unwrap().chars().count();
+        let mut x = 0;
+        while x < char_count {
+            let char = input.lines().nth(y).unwrap().chars().nth(x).unwrap();
+            if char.is_numeric()  {
+                if let Some((gear, x1, y1, x2, y2)) = get_boundary_coordinates(input, x, y) {
+                    if let Some((x1, y1)) = is_potential_gear(input, x1, y1, x2, y2) {
+                        if !potential_gear_map.contains_key(&(x1, y1)) {
+                            potential_gear_map.insert((x1, y1), gear);
+                        } else {
+                            calibration_sum += gear * potential_gear_map.get(&(x1, y1)).unwrap();
+                        }
+                    }
+                    if x2 + 1 >= char_count {
+                        break;
+                    }
+                    x = x2;
+                    continue;
+                }
+            }
+            x += 1;
+        }
+    }
+
+    if calibration_sum == 0 {
+        Err(std::io::Error::new(std::io::ErrorKind::Other, "No part found"))
+    } else {
+        Ok(calibration_sum)
     }
 }
 
@@ -70,10 +106,35 @@ fn is_part(input: &str, x1: usize, y1: usize, x2: usize, y2: usize) -> bool {
     false
 }
 
+fn is_potential_gear(input: &str, x1: usize, y1: usize, x2: usize, y2: usize) -> Option<(usize, usize)> {
+    for y in y1..=y2 {
+        for x in x1..=x2 {
+            let char = input.lines().nth(y).unwrap().chars().nth(x).unwrap();
+            if char == '*' {
+                return Some((x, y));
+            }
+        }
+    }
+    None
+}
+
 #[cfg(test)]
 mod test {
     use indoc::indoc;
     use super::*;
+
+    #[test]
+    fn test_is_potential_gear() {
+        let input = indoc! {"
+                .......
+                .10*...
+                .......
+                "};
+        if let Some((part, x1, y1, x2, y2)) = get_boundary_coordinates(input, 0, 0) {
+            assert!(is_potential_gear(input, x1, y1, x2, y2).is_some());
+            assert_eq!(part, 10);
+        }
+    }
 
     #[test]
     fn test_fetch_number_and_adjacent_grid() {
@@ -420,5 +481,24 @@ mod test {
         let result = day_three_part_one(&input);
         assert!(result.is_ok());
         assert_eq!(result.unwrap(), 4361);
+    }
+
+    #[test]
+    fn test_day_three_part_two_sample() {
+        let input = indoc! {"
+                    467..114..
+                    ...*......
+                    ..35..633.
+                    ......#...
+                    617*......
+                    .....+.58.
+                    ..592.....
+                    ......755.
+                    ...$.*....
+                    .664.598..
+                    "};
+        let result = day_three_part_two(&input);
+        assert!(result.is_ok());
+        assert_eq!(result.unwrap(), 467835);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ mod day_three;
 
 use day_one::{ day_one_part_one, day_one_part_two };
 use day_two::{ day_two_part_one, day_two_part_two };
-use day_three::{ day_three_part_one };
+use day_three::{ day_three_part_one, day_three_part_two };
 
 fn load_file(path: &str) -> Result<String, std::io::Error> {
     std::fs::read_to_string(path)
@@ -24,6 +24,7 @@ fn main() {
     let input = load_file("day_three_input")
         .unwrap_or_else(|_| panic!("Could not load file"));
     println!("day three part one: {}", day_three_part_one(&input).unwrap());
+    println!("day three part two {}", day_three_part_two(&input).unwrap());
 }
 
 #[cfg(test)]
